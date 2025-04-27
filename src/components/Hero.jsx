a91b893f2846b7e19be8b78598006e623a0aa60a
Rajnandini <rajnandinipatra2003@gmail.com>
2025-04-27 18:32:36 +0530

    Solving CV Error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import imghero from "/imghero.png";
 import github from "/github.png";
 import linkedin from "/linkedin.png";
-import Resume from "/Resume_Rajandini.pdf";
+import Resume from "/Resume_Rajnandini.pdf";
 
 export default function Hero() {
   return (
